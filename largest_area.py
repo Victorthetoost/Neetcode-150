31d2bfe5210b28193dd2d5bf68f87591ebdba400
Victor <vitya.voznyuk@gmail.com>
2026-03-12 00:55:32 -0400

    holy fuck im just the best?

---
+++ largest_area.py
@@ -0,0 +1,26 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        # goes through the list
+        # when it sees a number smaller than it, it stops
+        # goes to the left same thing
+        # saves sum if its the biggest
+        biggest = 0
+        for i in range(0,len(heights)):
+            # right:
+            j = i
+            area = 0
+            while j < len(heights):
+                if heights[j] >= heights[i]:
+                    area = area + heights[i]
+                else: break
+                j = j + 1
+            j = i - 1
+            while j >= 0 :
+                if heights[j] >= heights[i]:
+                    area = area + heights[i]
+                else: break
+                j = j - 1
+            if area > biggest:
+                biggest = area
+        return biggest
+
